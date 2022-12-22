6f572e5c986fed00fcaf8fb4eaa3e9930f2af1fc
Alan Xu <alan.xu1699@gmail.com>
2022-12-21 22:55:48 -0500

    set up stock question and empty function

---
+++ data_structures_algorithms/best_time_to_buy_sell.js
@@ -0,0 +1,3 @@
+// You are given an integer array prices where prices[i] is the price of a given stock on the i^th day.
+// On each day, you may decide to buy/sell the stock. You can hold at most one share of the stock at any time. However you can buy it then immediately sell it on the same day.
+// Find and return the maximum profit you can achieve.
