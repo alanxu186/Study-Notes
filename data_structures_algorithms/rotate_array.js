6581019bb4a78c592e800a70ecd9cac9db11e43c
Alan Xu <alan.xu1699@gmail.com>
2022-12-24 00:03:03 -0500

    set up problem and function to rotate array

---
+++ data_structures_algorithms/rotate_array.js
@@ -0,0 +1,10 @@
+//Given an array, rotate the array to the right by k steps, where k is non-negative
+
+/**
+ * @param{number[]}nums
+ * @return{number}
+ */
+
+var findMin = function (nums) {
+
+};
