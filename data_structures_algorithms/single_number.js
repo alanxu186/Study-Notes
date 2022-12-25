9b57c34fa8615481b91934a9b18720ea5f4a4e6f
Alan Xu <alan.xu1699@gmail.com>
2022-12-24 21:48:30 -0500

    set up single number question

---
+++ data_structures_algorithms/single_number.js
@@ -0,0 +1,11 @@
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one. 
+//Must have linear runtime complexity and use constant extra space
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var singleNumber = function(nums){
+
+};
